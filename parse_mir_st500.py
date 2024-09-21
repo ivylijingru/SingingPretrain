3cf80a06f98da34ab84d04d00296dfba2efc17a6
ivylijingru <1084309295@qq.com>
2024-09-21 10:30:10 -0400

    dataset

---
+++ parse_mir_st500.py
@@ -0,0 +1,39 @@
+import json
+import os
+from tqdm import tqdm
+import soundfile as sf
+
+import warnings
+import librosa
+import numpy as np
+import pandas as pd
+
+
+def parse_mirst500(data_split, output_dir):
+    label_dir = "MERT-label"
+    feature_dir = "MERT-v0-public_feature_default"
+
+    data_list = []
+    for the_dir in tqdm(os.listdir(os.path.join(label_dir, data_split))):
+        clip_id = int(the_dir)
+        mert_path = os.path.join(feature_dir, data_split, the_dir, "Vocals.wav.npy")
+        label_path = os.path.join(label_dir, data_split, the_dir, "labels.npy")
+
+        data = dict(
+            clip_id=clip_id,
+            label_path=label_path,
+            mert_path=mert_path,
+        )
+
+        data_list.append(data)
+    
+    with open(os.path.join(output_dir, "mirst500-{}.json".format(data_split)), "w") as f:
+        for data in data_list:
+            json.dump(data, f)
+            f.write('\n')
+            f.flush()
+
+
+if __name__ == '__main__':
+    parse_mirst500("train", "data_json")
+    parse_mirst500("test", "data_json")
