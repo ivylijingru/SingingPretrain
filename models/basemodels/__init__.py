a52f0de1c9b42064f28bfafb03b9f00bf962cab7
ivylijingru <1084309295@qq.com>
2024-09-21 11:29:50 -0400

    Update model & loss

--- models/basemodels/__init__.py
+++ models/basemodels/__init__.py
@@ -1,1 +1,7 @@
 from .downstream_mlp import DownstreamMLP
+
+
+def get_base_model(cfg: dict):
+    model = DownstreamMLP(**cfg["args"])
+
+    return model
--- models/loss_fn/__init__.py
+++ models/loss_fn/__init__.py
@@ -1,1 +1,5 @@
 from .combine_loss import CombineLoss
+
+
+def get_loss(cfg: dict):
+    return CombineLoss(**cfg["args"])
